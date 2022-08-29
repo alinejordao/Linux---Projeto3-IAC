9e418901a33508d9622daa66863cb445ce94833f
alinejordao <alinejordao01@gmail.com>
2022-08-28 23:30:29 -0300

    arquivos do Iac v.3

--- Arquivos/index.php
+++ Arquivos/index.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-<title>Exemplo PHP</title>
+<title>Exemplo  de PHP</title>
 </head>
 <body>
 
@@ -16,7 +16,7 @@ echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 $servername = "54.234.153.24";
 $username = "root";
 $password = "Senha123";
-$database = "meubanco";
+$database = "MyBD";
 
 // Criar conexão
 
